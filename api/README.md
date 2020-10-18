73c14fbd09db31e347545694b81c0bf7ac44b6f2
Tyler Gasperlin <gasperlint@gmail.com>
2020-10-18 15:58:28 -0700

    Initial commit neo4j example get data

---
+++ api/README.md
@@ -0,0 +1,1 @@
+- To Run: node app
---
+++ api/app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const neo4j = require('neo4j-driver')
+
+const app = express();
+
+// view js will be view engine 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123456'))
+const session = driver.session()
+
+// home route
+app.get('/', (req, res) => {
+    session
+        .run("MATCH (n: Person) RETURN n")
+        .then((result) => {
+            let personArr = []
+            result.records.forEach( item => {
+                console.log(item._fields[0])
+                personArr.push({
+                    id: item._fields[0].identity.low,
+                    name: item._fields[0].properties.name
+                })
+            })
+            res.render('index', {
+                persons: personArr
+            })
+
+        })
+        .catch( err => {
+            console.log(err)
+        })
+        
+})
+
+app.listen(3000)
+
+console.log(
+    ' server started on port 3000'
+)
+
+module.exports = app;
